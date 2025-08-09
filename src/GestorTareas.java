b8b1311a2c21d7754ac95f2ee33e5b8c67ebbba9
Oscar Oliva <yahiroliva97@gmail.com>
2025-08-08 20:02:30 -0600

    feat: se agregan atributos, construcotres y getters y setters a las clases

--- src/GestorTareas.java
+++ src/GestorTareas.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GestorTareas {
+    // Atributos Lista de tareas
+
+    private ArrayListList<Tarea> Listatareas;
+
+    // Constructor de la clase GestorTareas
+    public GestorTareas() {
+        this.Listatareas = new ArrayListList<Tarea>();
+    }
 }
--- src/Tarea.java
+++ src/Tarea.java
@@ -1,3 +1,50 @@
 public class Tarea {
+    // Atributos de la clase Tarea
+    private String descripcion;
+    private String fechalimite;
+    private String prioridad; // alta, media, baja
+    private String categoria; // personal, trabajo, estudio, otro
+    private boolean completada;
 
+    // Constructor de la clase Tarea
+    public Tarea(String descripcion, String fechalimite, String prioridad, String categoria) {
+        this.descripcion = descripcion;
+        this.fechalimite = fechalimite;
+        this.prioridad = prioridad;
+        this.categoria = categoria;
+        this.completada = false; // Por defecto, la tarea no está completada
+    }
+
+    // Getters y Setters
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public String getFechalimite() {
+        return fechalimite;
+    }
+    public void setFechalimite(String fechalimite) {
+        this.fechalimite = fechalimite;
+    }
+    public String getPrioridad() {
+        return prioridad;
+    }
+    public void setPrioridad(String prioridad) {
+        this.prioridad = prioridad;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public boolean isCompletada() {
+        return completada;
+    }
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
 }
